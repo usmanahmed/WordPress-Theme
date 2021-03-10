efc5b9f647c6fb55c0ea35fe548fd6a0132c8010
Usman Ahmed <9985034+usmanahmed@users.noreply.github.com>
2021-03-10 20:03:21 +0500

    Proceedings

--- wp-content/themes/custom-theme/functions/theme-setup.php
+++ wp-content/themes/custom-theme/functions/theme-setup.php
@@ -6,5 +6,5 @@ define('PARTIALS', TEMPLATE_DIR_PATH . '/partials/');
 
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('style', get_stylesheet_uri(), false, filemtime(TEMPLATE_DIR_PATH . '/style.css'), 'all'); // links to default style.css
-    wp_enqueue_script('main', ASSETS . 'js/main.js', false, filemtime(TEMPLATE_DIR_PATH . '/assets/js/main.js'), true);
+    wp_enqueue_script('main', ASSETS . 'js/main.js', ['jquery'], filemtime(TEMPLATE_DIR_PATH . '/assets/js/main.js'), true);
 });
