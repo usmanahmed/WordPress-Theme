b94277b434761aec60ff38bb28b861434df05e38
Usman Ahmed <9985034+usmanahmed@users.noreply.github.com>
2021-03-10 21:55:21 +0500

    Hooks names added

--- wp-content/themes/custom-theme/functions/wp-hooks.php
+++ wp-content/themes/custom-theme/functions/wp-hooks.php
@@ -1,2 +1,41 @@
 <?php
 
+/*
+    **** Actions ****
+    init
+    after_setup_theme
+    template_redirect
+    wp_head
+    
+    wp_ajax_{$action}
+    add_meta_boxes
+    admin_bar_menu
+    admin_enqueue_scripts
+    admin_menu
+    {$taxonomy}_edit_form_fields
+    created_{$taxonomy}
+    edited_{$taxonomy}
+    manage_{$post->post_type}_posts_custom_column
+    manage_posts_custom_column
+    publish_post
+    restrict_manage_posts
+    save_post
+    wp_login
+
+
+    **** Filters ****
+    body_class
+    parse_query
+    document_title_parts
+    query_vars
+    mime_types
+
+    file_is_displayable_image
+    manage_posts_columns
+    manage_{$post_type}_posts_columns
+    manage_{$this->screen->id}_sortable_columns
+    request
+    post_link
+    term_link
+    login_url
+*/
