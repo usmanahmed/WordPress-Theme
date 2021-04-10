34f16eca023a963da8d0bb1c193691bb939b2407
Usman Ahmed <9985034+usmanahmed@users.noreply.github.com>
2021-04-10 09:19:41 +0500

    Theme Support Added

--- wp-content/themes/custom-theme/functions.php
+++ wp-content/themes/custom-theme/functions.php
@@ -3,3 +3,4 @@
     include 'functions/wp-hooks.php';
     include 'functions/ajax.php';
     include 'functions/pt-and-tax.php';
+    include 'functions/admin.php';
--- wp-content/themes/custom-theme/functions/theme-setup.php
+++ wp-content/themes/custom-theme/functions/theme-setup.php
@@ -8,3 +8,22 @@ add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('style', get_stylesheet_uri(), false, filemtime(TEMPLATE_DIR_PATH . '/style.css'), 'all'); // links to default style.css
     wp_enqueue_script('main', ASSETS . 'js/main.js', ['jquery'], filemtime(TEMPLATE_DIR_PATH . '/assets/js/main.js'), true);
 });
+
+
+add_action( 'after_setup_theme', function() {
+    $theme_support = [
+      'post-formats',
+      'post-thumbnails',
+      'html5',
+      'custom-logo',
+      'custom-header-uploads',
+      'custom-header',
+      'custom-background',
+      'title-tag',
+      'starter-content',
+      'responsive-embeds'
+    ];
+    foreach ($theme_support as $item) {
+      add_theme_support( $item );
+    }
+  });
